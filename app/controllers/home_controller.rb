d324766d27ad3309ce0048277c490e3f85fe1ef9
Scott Murphy <scott@standardco.de>
2016-01-08 13:02:00 -0500

    commented out truevault logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    truevault = ::TrueVault::Client.new
-    doc_list = truevault.documents_index(ENV['TV_TEST_VAULT'])
-
-    @docs = Array.new
-
-    doc_list[:data][:items].each do |d|
-      @docs << d[:id]
-    end
 
   end
 
--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -1,39 +1,38 @@
 class SubmissionController < ApplicationController
 
   def submission
-    identifiers = ["name", "age", "gender"]
-    non_identifiers = ["left_eye_tf", "right_eye_tf"]
-
-    truevault = ::TrueVault::Client.new
-
-    data = params[:xml_submission_file]
-
-    xml = File.open(data.open).read
-
-    hash = Hash.from_xml(xml)
-
-    puts '!-----@@-----!'
-    pg_data = hash["data"]
-    tv_data = hash["data"]
-
-    identifiers.each do |i|
-      tv_data = tv_data.dup.except!("#{i}")
-    end
-
-    non_identifiers.each do |i|
-      pg_data = pg_data.dup.except!("#{i}")
-    end
-
-    puts '!-----@@-----!'
-
-    response = truevault.create_document(ENV['TV_TEST_VAULT'], tv_data)
+    # identifiers = ["name", "age", "gender"]
+    # non_identifiers = ["left_eye_tf", "right_eye_tf"]
+    #
+    # truevault = ::TrueVault::Client.new
+    #
+    # data = params[:xml_submission_file]
+    #
+    # xml = File.open(data.open).read
+    #
+    # hash = Hash.from_xml(xml)
+    #
+    # puts '!-----@@-----!'
+    # pg_data = hash["data"]
+    # tv_data = hash["data"]
+    #
+    # identifiers.each do |i|
+    #   tv_data = tv_data.dup.except!("#{i}")
+    # end
+    #
+    # non_identifiers.each do |i|
+    #   pg_data = pg_data.dup.except!("#{i}")
+    # end
+    #
+    # puts '!-----@@-----!'
+    #
+    # response = truevault.create_document(ENV['TV_TEST_VAULT'], tv_data)
 
     render nothing: true
   end
 
   def view_doc
-    truevault = ::TrueVault::Client.new
-    @response = truevault.show_document(ENV['TV_TEST_VAULT'], params[:id])
+
   end
 
   def postgres_submission
