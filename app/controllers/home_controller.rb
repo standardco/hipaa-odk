22b29a83dfb668000d313d52775ea99854ad8f40
Scott Murphy <scott@standardco.de>
2016-01-27 14:28:22 -0500

    debugging auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   end
 
   def forms
-    puts request.headers['User-Agent']
+    puts request.headers.inspect.to_yaml
 
     # These two headers are required by ODK Collect to pull in the form list
     response.headers['X-OpenRosa-Version'] = '1'
