7651791e6a6b4066a6d36b9ab210453fb17b6e8d
Scott Murphy <scott@standardco.de>
2016-01-27 14:24:10 -0500

    added response outputs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,8 @@ class HomeController < ApplicationController
   end
 
   def forms
-    
+    puts request.headers['User-Agent']
+
     # These two headers are required by ODK Collect to pull in the form list
     response.headers['X-OpenRosa-Version'] = '1'
     response.headers['Content-Type'] = 'text/xml; charset=utf-8'
