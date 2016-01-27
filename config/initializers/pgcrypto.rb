79917a279c1d0490809fb362f5e0ea0ab9b651fa
Scott Murphy <scott@standardco.de>
2016-01-27 13:15:40 -0500

    setup logic for pulling in keys per environment

--- config/initializers/pgcrypto.rb
+++ config/initializers/pgcrypto.rb
@@ -1,3 +1,7 @@
-# Uncomment the line below and point it to your private key
-PGCrypto.keys[:private] = {:path => '/Users/murph/Desktop/secret.key', armored: true, password: ENV['PGC_PRIVATE_PASSWORD']}
-PGCrypto.keys[:public] = {:path => '/Users/murph/Desktop/public.key'}
+if Rails.env.production?
+  PGCrypto.keys[:private] = {:value => ENV['PRIVATE_KEY'], armored: true, password: ENV['PRIVATE_KEY_PASSWORD']}
+  PGCrypto.keys[:public] = {:value => ENV['PUBLIC_KEY']}
+else
+  PGCrypto.keys[:private] = {:path => '/Users/murph/Desktop/secret.key', armored: true, password: ENV['PRIVATE_KEY_PASSWORD']}
+  PGCrypto.keys[:public] = {:path => '/Users/murph/Desktop/public.key'}
+end
